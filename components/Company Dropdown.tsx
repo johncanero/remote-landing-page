16f11a8fe5168c48647a0ba902e80712b79d3d3a
John Canero <johncanero.official@gmail.com>
2022-10-13 12:22:30 +0800

    Navbar Modify Styling of Spacing Sublinks

--- components/Company Dropdown.tsx
+++ components/Company Dropdown.tsx
@@ -12,7 +12,7 @@ export default function Example() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center bg-white ml-5 py-2 text-lg font-medium text-gray-600">
+        <Menu.Button className="inline-flex w-full bg-white ml-5 py-2 text-lg font-medium text-gray-600">
             Company
           <ChevronDownIcon className="ml-1 mt-1 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
@@ -27,7 +27,7 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute z-10 mt-2 w-56 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute z-10 mt-2 w-36 ml-5 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
