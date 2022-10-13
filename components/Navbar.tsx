235ba4f30a07f0606becd51e06498acfe12b3500
John Canero <johncanero.official@gmail.com>
2022-10-13 14:57:57 +0800

    React Icons Install

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BellIcon, XMarkIcon, } from "@heroicons/react/24/outline";
+
+
 import Link from "next/link";
 import Image from "next/image";
 import FeaturesDropdown from "./FeaturesDropdown";
