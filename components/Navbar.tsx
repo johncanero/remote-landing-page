dcec9ddb7899c61cee02bb4b5d5f711f41eeed52
John Canero <johncanero.official@gmail.com>
2022-10-13 11:38:06 +0800

    Next Link in Logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import Image from "next/image";
-import FeaturesDropdown from './FeaturesDropdown'
-import CompanyDropdown from './Company Dropdown'
+import FeaturesDropdown from "./FeaturesDropdown";
+import CompanyDropdown from "./Company Dropdown";
 import RemoteLogo from "../public/images/remoteLogo.png";
-
-
-
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -23,7 +21,6 @@ const Navbar = () => {
             <div className="mx-auto px-2 sm:px-6 lg:px-8 shadow-md">
               <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-
                   {/* Mobile menu button*/}
                   <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
@@ -35,23 +32,21 @@ const Navbar = () => {
                   </Disclosure.Button>
                 </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-
                   <div className="flex flex-shrink-0 h-8 w-40 mt-1 items-center">
                     {/* Image */}
-                    <Image
-                      className="block h-8 w-3 lg:hidden"
-                      src={RemoteLogo}
-                      alt="Your Company"
-                    />
+                    <Link href="/" passHref>
+                      <Image
+                        className="block h-8 w-3 lg:hidden"
+                        src={RemoteLogo}
+                        alt="Your Company"
+                      />
+                    </Link>
                   </div>
 
-                   {/* Links */}
-                  <FeaturesDropdown  />
+                  {/* Links */}
+                  <FeaturesDropdown />
                   <CompanyDropdown />
-                  
                 </div>
-
-               
 
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   <button
