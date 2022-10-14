7913e89de4c3945f5e6f4a915442add15c5c2f7a
John Canero <johncanero.official@gmail.com>
2022-10-15 00:50:56 +0800

    Dropdown Mobile

--- components/MobileDropdown.tsx
+++ components/MobileDropdown.tsx
@@ -1,23 +1,19 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { FaClipboardList } from '@react-icons/all-files/fa/FaClipboardList'
-import { FaCalendarAlt } from '@react-icons/all-files/fa/FaCalendarAlt'
-import { FaClipboard } from '@react-icons/all-files/fa/FaClipboard'
-import { BsGearFill } from '@react-icons/all-files/bs/BsGearFill'
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { GoThreeBars } from "@react-icons/all-files/go/GoThreeBars";
+import FeaturesDropdown from "./FeaturesDropdown";
+import CompanyDropdown from "./CompanyDropdown";
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
-
 
 export default function Example() {
   return (
-    <Menu as="div" className=" relative inline-block text-left">
+    <Menu as="div" className=" relative inline-block">
       <div>
-        <Menu.Button className="ml-12 inline-flex w-full bg-white md:ml-6 py-2 text-base font-Poppins font-medium text-gray-600 hover:text-gray-800">
-            Features
-          <ChevronDownIcon className="ml-1 mt-1 h-5 w-5" aria-hidden="true" />
+        <Menu.Button className="inline-flex w-full bg-white  py-2 text-base font-Poppins font-medium text-gray-600 hover:text-gray-800">
+          <GoThreeBars className=" h-11 w-5" />
         </Menu.Button>
       </div>
 
@@ -31,18 +27,18 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute z-10 mt-2 w-36 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="mt-96 z-10 mt-2 w-44 h-96 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-600',
-                    'flex px-4 py-2 text-base font-Poppins font-medium'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-600",
+                    "flex px-4 py-2 text-base font-Poppins font-medium"
                   )}
                 >
-                  <FaClipboardList className='mt-1 mr-1 text-remoteOrange' /> ToDo List
+                  <FeaturesDropdown />
                 </a>
               )}
             </Menu.Item>
@@ -51,11 +47,11 @@ export default function Example() {
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-600',
-                    'flex px-4 py-2 text-base font-Poppins font-medium'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-600",
+                    "flex px-4 text-base font-Poppins font-medium"
                   )}
                 >
-                 <FaCalendarAlt className='mt-1 mr-1 text-remoteBlue'/>  Calendars
+                  <CompanyDropdown />
                 </a>
               )}
             </Menu.Item>
@@ -64,11 +60,11 @@ export default function Example() {
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-600',
-                    'flex px-4 py-2 text-base font-Poppins font-medium'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-600",
+                    "flex px-4 py-2 mt-1 text-base font-Poppins font-medium"
                   )}
                 >
-                  <FaClipboard className='mt-1 mr-1 text-remoteLightBrown'/> Reminders
+
                 </a>
               )}
             </Menu.Item>
@@ -78,12 +74,10 @@ export default function Example() {
                   <button
                     type="submit"
                     className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-600',
-                      'flex w-full px-4 py-2 text-left text-base font-Poppins font-medium'
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-600",
+                      "flex w-full px-4 py-2 text-left text-base font-Poppins font-medium"
                     )}
-                  >
-                    <BsGearFill className='mt-1 mr-1 text-slate-700'/> Planning
-                  </button>
+                  ></button>
                 )}
               </Menu.Item>
             </form>
@@ -91,5 +85,5 @@ export default function Example() {
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
