6b9a92eb80364a46e819b9d5406488b01b9d74a3
John Canero <johncanero.official@gmail.com>
2022-10-14 13:04:09 +0800

    Mobile Dropdown

--- components/MobileDropdown.tsx
+++ components/MobileDropdown.tsx
@@ -13,9 +13,9 @@ function classNames(...classes: string[]) {
 
 export default function Example() {
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className=" relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full bg-white md:ml-6 py-2 text-base font-Poppins font-medium text-gray-600 hover:text-gray-800">
+        <Menu.Button className="ml-12 inline-flex w-full bg-white md:ml-6 py-2 text-base font-Poppins font-medium text-gray-600 hover:text-gray-800">
             Features
           <ChevronDownIcon className="ml-1 mt-1 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
