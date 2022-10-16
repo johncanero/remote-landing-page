767e0385f5cd98edff5423161815afb3bfa10629
John Canero <johncanero.official@gmail.com>
2022-10-16 15:14:21 +0800

    Adjust Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,29 +20,29 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className=" sm:mx-8 md:mx-20 lg:mx-24">
+        <div className=" sm:mx-8 md:mx-20 lg:mx-20">
           <h1 className="font-semibold text-5xl md:mt-4 md:text-7xl lg:text-8xl lg:mt-32 ">
             Make remote work<span className="text-remoteOrange">.</span>
             <span className="text-remoteBlue">.</span>
             <span className="text-remoteLightBrown">.</span>
           </h1>
 
-          <p className="font-medium text-gray-500 text-lg mt-8 md:mr-56 md:mt-14 lg:mt-16 ">
+          <p className="font-medium text-gray-500 text-lg mt-8 md:mr-56 md:mt-14 lg:mt-14 ">
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
           </p>
 
-          <button className=" bg-remoteOrange text-gray-800 font-semibold hover:bg-gray-800 hover:text-white py-2 px-4 hover:border-transparent rounded-xl mt-8  md:mt-14">
+          <button className=" bg-remoteOrange text-gray-800 font-semibold hover:bg-gray-800 hover:text-white py-2 px-4 hover:border-transparent rounded-xl mt-8  md:mt-12">
             Learn more
           </button>
         </div>
 
         {/* Desktop */}
-        <div className="sm:hidden md:hidden lg:block lg:mx-18 lg:mr-12 lg:mt-6">
+        <div className="sm:hidden md:hidden lg:block lg:mx-18 lg:mr-14 lg:mt-10">
           <div className="">
             <Image
-              width={1650}
-              height={1220}
+              width={1700}
+              height={1320}
               className="block"
               src={HeroImage}
               alt="Hero Feature"
