212469eb6499e635c912ac1670b5cdff1a9be229
John Canero <johncanero.official@gmail.com>
2022-10-18 11:48:05 +0800

    Change Footer Background and Target Blank Anchor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="fixed inset-x-0 bottom-0 bg-gray-100 text-center lg:text-left">
-        <div
-          className="text-gray-700 text-center p-3"
-        >
+      <footer className="fixed inset-x-0 bottom-0 bg-gray-900 text-center lg:text-left">
+        <div className="text-white text-center p-3">
           © 2021 Copyright:
-          <a className="text-gray-800 ml-1 font-semibold" href="https://www.linkedin.com/in/johncanero/">
+          <a
+            className="text-white ml-1 font-semibold"
+            target="_blank"
+            href="https://www.linkedin.com/in/johncanero/"
+            rel="noopener noreferrer"
+          >
             John Cañero
           </a>
         </div>
