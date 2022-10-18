ca0aa42d961e3aecfd240159b6d618be72d7eb1b
John Canero <johncanero.official@gmail.com>
2022-10-18 11:52:16 +0800

    Footer Background Modify Styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="fixed inset-x-0 bottom-0 bg-gray-900 text-center lg:text-left">
-        <div className="text-white text-center p-3">
+      <footer className="fixed inset-x-0 bottom-0 bg-gray-50 text-center lg:text-left">
+        <div className="text-gray-800 text-center p-3">
           © 2021 Copyright:
           <a
-            className="text-white ml-1 font-semibold"
+            className=" text-gray-800 ml-1 font-semibold"
             target="_blank"
             href="https://www.linkedin.com/in/johncanero/"
             rel="noopener noreferrer"
