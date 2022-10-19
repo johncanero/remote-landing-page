c73995ccf08ab959a9fb923374003a338a90c7f5
John Canero <johncanero.official@gmail.com>
2022-10-19 17:07:51 +0800

    2022 Copyright Modify

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <footer className="fixed inset-x-0 bottom-0 bg-gray-100 text-center lg:text-left">
         <div className="text-gray-800 text-center p-3">
-          © 2021 Copyright:
+          © 2022 Copyright:
           <a
             className=" text-gray-800 ml-1 font-semibold"
             target="_blank"
