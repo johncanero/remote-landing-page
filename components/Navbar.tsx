6e5d595b0182ed16a5817d4894cc421ba8682c04
John Canero <johncanero.official@gmail.com>
2022-11-20 23:02:09 +0800

    Dark Mode for Mobile Header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -76,7 +76,7 @@ const Navbar = () => {
         {({ open }) => (
           <div>
             {/* Mobile */}
-            <div className="md:hidden sm:block mx-auto px-2 sm:px-6 lg:px-8 shadow-sm">
+            <div className="md:hidden sm:block mx-auto px-2 sm:px-6 lg:px-8 shadow-sm dark:bg-black">
               <div className="relative flex h-16 items-center justify-between">
                 <div className="flex flex-shrink-0 w-36 items-center">
                   <Link href="/" passHref>
