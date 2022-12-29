0a2888be7351675e29ef5bad6e840d116f8a702b
John Canero <johncanero.official@gmail.com>
2022-12-30 00:18:00 +0800

    Change Font Style and Size

--- pages/company/index.tsx
+++ pages/company/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
         <div>
             <Navbar />
             <div>
-                <h1 className="flex justify-center mt-6 text-2xl font-bold lg:text-left md:text-3xl font-Montserrat text-neutral-600 dark:text-neutral-400">
+                <h1 className="flex justify-center mt-6 text-2xl font-bold lg:text-left font-Poppins text-neutral-600 dark:text-neutral-400">
                     Company
                 </h1>
             </div>
