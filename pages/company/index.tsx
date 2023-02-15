7a5f2d44c8b3a77f446561b8451cda995d6d85f4
John Canero <johncanero.official@gmail.com>
2023-02-15 17:57:09 +0800

    Add Font-Medium in Titles on the Company Page

--- pages/company/index.tsx
+++ pages/company/index.tsx
@@ -23,7 +23,7 @@ const index = () => {
                             the remote work company that's committed to providing a flexible and fulfilling work experience for our team members. We believe that remote work is the future of work, and we're proud to be leading the way in creating a culture of remote work excellence.
                         </p>
 
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Our Commitment to Remote Work:
                         </p>
 
@@ -31,7 +31,7 @@ const index = () => {
                             At Remote, remote work is not just an option - it's a core part of our company philosophy. We believe that remote work allows our team members to be more productive, creative, and engaged in their work, which ultimately leads to better outcomes for our clients and our company.
                         </p>
 
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
 
                             Available Remote Work Options:
                         </p>
@@ -41,7 +41,7 @@ const index = () => {
                             We offer a variety of remote work options to fit the needs of our team members. Whether you're looking for a fully remote position, a partial remote arrangement, or a flexible remote schedule, we're here to support you in finding the right work arrangement to fit your lifestyle.
                         </p>
 
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Communication and Collaboration Tools:
                         </p>
 
@@ -49,7 +49,7 @@ const index = () => {
                             We use the latest communication and collaboration tools to keep our remote team members connected and engaged. From project management software to video conferencing tools and messaging apps, we make sure that our team members have everything they need to stay connected and productive.
                         </p>
 
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Remote Work Policies:
                         </p>
 
@@ -57,7 +57,7 @@ const index = () => {
                             Our remote work policies are designed to ensure that our team members have the support they need to be successful in their roles. We provide guidelines for communication, productivity, and availability, so that everyone is on the same page and working towards the same goals.
                         </p>
 
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Onboarding and Training for Remote Team Members:
                         </p>
 
@@ -66,7 +66,7 @@ const index = () => {
                             We understand that starting a new remote job can be challenging, which is why we provide comprehensive onboarding and training for all of our new remote team members. We make sure that you have the tools, processes, and knowledge you need to be successful in your role.
                         </p>
 
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Support for Remote Team Members:
                         </p>
 
@@ -75,19 +75,19 @@ const index = () => {
                         </p>
 
 
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Remote Team Culture:
                         </p>
                         <p className='mt-3 text-justify text-md'>
                             At Remote, we believe in fostering a positive team culture, no matter where our team members are located. We host team building activities, virtual events, and recognition programs to keep our team members connected and engaged.
                         </p>
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Testimonials from Current Remote Team Members:
                         </p>
                         <p className='mt-3 text-justify text-md'>
                             Don't just take our word for it - hear from some of our current remote team members about their experience working at Remote. They'll tell you about the benefits of working remotely, the support they receive from our company, and the positive team culture that we've created.
                         </p>
-                        <p className='mt-3 text-justify text-md dark:text-remoteLightBrown'>
+                        <p className='mt-3 font-medium text-justify text-md dark:text-remoteLightBrown'>
                             Join the Remote Work Revolution:
                         </p>
                         <p className='mt-3 text-justify text-md'>
